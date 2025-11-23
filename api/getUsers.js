273c87ccfa75963d8ecac434b448234950134a1f
gsteeiinn-code <gsteeiinn@gmail.com>
2025-11-23 01:20:58 -0300

    Update getUsers.js

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,1 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+    const usersPath = path.join(process.cwd(), "data", "users.json");
+    const users = JSON.parse(fs.readFileSync(usersPath, "utf8"));
+    return res.status(200).json(users);
+}
 
