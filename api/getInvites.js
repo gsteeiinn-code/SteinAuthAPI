b0cb6c548fdd84f9af3740181d58b6896e4d2e11
gsteeiinn-code <gsteeiinn@gmail.com>
2025-11-23 01:21:05 -0300

    Update getInvites.js

--- api/getInvites.js
+++ api/getInvites.js
@@ -1,1 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+    const invitesPath = path.join(process.cwd(), "data", "invites.json");
+    const invites = JSON.parse(fs.readFileSync(invitesPath, "utf8"));
+    return res.status(200).json(invites);
+}
 
